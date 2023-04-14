6592c808aab16756d58c2f05ff123894dad04550
Justina Wong <justinawong97@gmail.com>
2023-04-14 15:38:43 -0700

    additional UI edits

--- src/App.js
+++ src/App.js
@@ -3,36 +3,44 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  // const [totalClicks, setTotalClicks] = useState(0);
-  // const updateClicksHandler = () => {
-  //   setTotalClicks((totalClicks) => totalClicks + 1);
-  // };
-
   const [count, setCount] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const errorMessage = "Oops, error. Contact me!";
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     axios
       .get("/retrieve/")
       .then((res) => {
+        setIsLoading(false);
+        setHasError(false);
         setCount(res.data["count"]);
       })
-      .catch((err) => console.log(err));
+      .catch(() => setHasError(true));
   }, []);
 
   const buttonClick = async () => {
     axios
       .get("/increment/")
       .then((res) => {
+        setHasError(false);
         setCount(res.data["count"]);
       })
-      .catch((err) => console.log(err));
+      .catch(() => setHasError(true));
   };
+
+  let content = <div className="count">{count ? count : "No clicks, yet"}</div>;
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="spacing">{count ? count : "No clicks, yet"}</div>
-        <button onClick={buttonClick}>Increment here</button>
+        {isLoading && <p>Fetching clicks...</p>}
+        {!isLoading && content}
+        <button className="clickMe" onClick={buttonClick}>
+          Click me!
+        </button>
+        <p className="errorMsg">{hasError && errorMessage}</p>
       </header>
     </div>
   );
